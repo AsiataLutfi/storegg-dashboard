ede4fce5338397535099405e55b47bea70c1e06a
Asiata Lutfi <asiatakh25@gmail.com>
2022-06-24 14:46:20 +0700

    Getting Started

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      spacing: {
+        7.5: '30px',
+        12.5: '50px',
+      },
+    },
+  },
+  plugins: [],
+};
